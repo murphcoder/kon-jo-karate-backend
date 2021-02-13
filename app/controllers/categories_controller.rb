04e464229425dc0aa55fc4c31cb247c3f430969f
murphcoder <murphcoder@gmail.com>
2021-02-13 16:26:42 -0600

    Category controller first draft

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,45 @@
 class CategoriesController < ApplicationController
+
+    def show
+        @category = Category.find(params[:id])
+        if @category
+            render json: {caregory: @category}
+        else
+            render json: {status: 500, errors: ['category not found']}
+        end
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            render json: {status: 'created', category: @category}
+        else
+            render json: {status: 500, errors: @category.errors.full_messages}
+        end
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        @category.update(category_params)
+        if @category.valid?
+            render json: {status: 'updated', category: @category}
+        else
+            render json: {status: 500, errors: @category.errors.full_messages}
+        end
+    end
+
+    def destroy
+        @category = Category.find(params[:id])
+        if @category
+            @category.destroy
+            render json: {status: 'deleted'}
+        else
+            render json: {status: 500, errors: @category.errors.full_messages}
+        end
+    end
+
+    def category_params
+        params.require(:category).permit(:name)
+    end
+
 end
