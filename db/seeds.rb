60e1beef1dcc3eeaee258859e02ef2102a524276
murphcoder <murphcoder@gmail.com>
2021-02-28 22:04:58 -0600

    Added Patric Murphy to Teacher Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-sean = {
-    name: "Sean",
-    password: "Fabbed1973",
-    description: "Not an actual instructor, just the website administrator.",
-    cell_phone: "224-856-7926",
-    email: "murphcoder@gmail.com",
-    admin: true
-}
+teachers = [
+    {
+        name: "Sean",
+        password: "Fabbed1973",
+        description: "Not an actual instructor, just the website administrator.",
+        cell_phone: "224-856-7926",
+        email: "murphcoder@gmail.com",
+        admin: true
+    },
+    {
+        name: "Patric",
+        password: "Konjo1949",
+        description: "Owner and lead instructor."
+        cell_phone: "847-639-7001",
+        email: "murphsenseo@yahoo.com",
+        admin: true
+    }
+]
 
-Instructor.create(sean)
+Instructor.create(teachers)
